9aaa84f11c932f951f59c78f1ad4ef05012465c1
xnxborges <ns_borges93@yahoo.com.br>
2022-10-14 15:12:12 -0300

    Implementacao da classe controller da aplicacao

---
+++ src/main/java/br/com/rest_apis/controller/PersonController.java
@@ -0,0 +1,24 @@
+package br.com.rest_apis.controller;
+
+import br.com.rest_apis.model.Person;
+import br.com.rest_apis.service.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/person")
+public class PersonController {
+
+    @Autowired
+    private PersonService personService;
+
+    @RequestMapping(value = "?{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Person findById(@PathVariable(value = "id") String id) throws Exception{
+        return personService.findById(id);
+    }
+
+}
