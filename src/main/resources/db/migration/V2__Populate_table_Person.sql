2912cd312c9b37a6380bf28326f1af9769679888
xnxborges <ns_borges93@yahoo.com.br>
2022-10-17 17:43:10 -0300

    Adicionado arquivos de criacao e populacao da tabela person
    Signed-off-by: xnxborges <ns_borges93@yahoo.com.br>

---
+++ src/main/resources/db/migration/V2__Populate_table_Person.sql
@@ -0,0 +1,4 @@
+INSERT INTO `person` (`id`, `first_name`, `last_name`, `address`, `gender`) VALUES
+(3,'FirstName','lastName','address - address','YourGender'),
+(4,'FirstName1','lastName','address - address1','YourGender1'),
+(5,'FirstName2','lastName','address - address2','YourGender2');
