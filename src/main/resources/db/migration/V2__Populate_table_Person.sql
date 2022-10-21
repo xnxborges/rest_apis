1ea8911c29e22fc6aee607490ff0ae24675b28ed
xnxborges <ns_borges93@yahoo.com.br>
2022-10-21 19:15:47 -0300

    Novos dados persistidos na tabela person do banco de dados
    Signed-off-by: xnxborges <ns_borges93@yahoo.com.br>

--- src/main/resources/db/migration/V2__Populate_table_Person.sql
+++ src/main/resources/db/migration/V2__Populate_table_Person.sql
@@ -1,4 +1,6 @@
 INSERT INTO `person` (`id`, `first_name`, `last_name`, `address`, `gender`) VALUES
-(3,'FirstName','lastName','address - address','YourGender'),
-(4,'FirstName1','lastName','address - address1','YourGender1'),
-(5,'FirstName2','lastName','address - address2','YourGender2');
+(3,'Jose','Lourencço','Cotia - São Paulo','Male'),
+(4,'Maria','Joaquina','Vargem - São Paulo','Female'),
+(5,'Chico','Mario','Rio de Janeiro - Rio de Janeiro','Male');
+(6,'Leon','Nardo','Arraial - Rio de Janeiro','Male');
+(7,'Josefa','Santos','Salvador - Bahia','Female');
