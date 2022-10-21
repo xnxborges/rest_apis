71f6819e029710439e1b1f890a4e8d74975bf00b
xnxborges <ns_borges93@yahoo.com.br>
2022-10-21 19:42:16 -0300

    Novo schema de insercao e novos dados adicionados a tabela person
    Signed-off-by: xnxborges <ns_borges93@yahoo.com.br>

--- src/main/resources/db/migration/V2__Insert_Data_In_Person.sql
+++ src/main/resources/db/migration/V2__Insert_Data_In_Person.sql
@@ -1,6 +1,6 @@
 INSERT INTO `person` (`id`, `first_name`, `last_name`, `address`, `gender`) VALUES
 (3,'Jose','Lourencço','Cotia - São Paulo','Male'),
 (4,'Maria','Joaquina','Vargem - São Paulo','Female'),
-(5,'Chico','Mario','Rio de Janeiro - Rio de Janeiro','Male');
-(6,'Leon','Nardo','Arraial - Rio de Janeiro','Male');
-(7,'Josefa','Santos','Salvador - Bahia','Female');
+(5,'Chico','Mario','Rio de Janeiro - Rio de Janeiro','Male'),
+(6,'Leon','Nardo','Arraial - Rio de Janeiro','Male'),
+(7,'Josefa','Santos','Salvador - Bahia','Female')
