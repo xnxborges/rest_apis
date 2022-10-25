a8e5d42e58a862d077de88c5fb979bb95072435a
xnxborges <ns_borges93@yahoo.com.br>
2022-10-25 09:21:17 -0300

    Implementada a classe de testes SwaggerIntegrationTest
    Signed-off-by: xnxborges <ns_borges93@yahoo.com.br>

---
+++ src/test/java/br/com/rest_apis/integrationTest/swagger/SwaggerIntegrationTest.java
@@ -0,0 +1,27 @@
+package br.com.rest_apis.integrationTest.swagger;
+
+import br.com.rest_apis.config.TestConfig;
+import br.com.rest_apis.integrationTest.testContainers.AbstractIntegrationTest;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+public class SwaggerIntegrationTest extends AbstractIntegrationTest {
+
+    @Test
+    public void shouldDisplaySwaggerUiPage() {
+        var content =
+                given()
+                        .basePath("/swagger-ui/index.html")
+                        .port(TestConfig.SERVER_PORT)
+                        .when()
+                        .get()
+                        .then().statusCode(200)
+                        .extract().body().asString();
+
+        assertTrue(content.contains("Swagger UI"));
+    }
+}
