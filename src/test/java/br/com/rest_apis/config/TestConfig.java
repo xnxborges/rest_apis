79f9ba78404dfa6975d779aa0d228af720cb3615
xnxborges <ns_borges93@yahoo.com.br>
2022-11-07 19:33:05 -0300

    Atualização nos arquivos de configuração.
    Signed-off-by: xnxborges <ns_borges93@yahoo.com.br>

--- src/test/java/br/com/rest_apis/config/TestConfig.java
+++ src/test/java/br/com/rest_apis/config/TestConfig.java
@@ -1,6 +1,5 @@
 package br.com.rest_apis.config;
 
 public class TestConfig {
-
     public static final int SERVER_PORT = 8888;
 }
--- src/test/java/br/com/rest_apis/integrationTest/swagger/SwaggerIntegrationTest.java
+++ src/test/java/br/com/rest_apis/integrationTest/swagger/SwaggerIntegrationTest.java
@@ -8,19 +8,22 @@ import org.springframework.boot.test.context.SpringBootTest;
 import static io.restassured.RestAssured.given;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public class SwaggerIntegrationTest extends AbstractIntegrationTest {
 
     @Test
     public void shouldDisplaySwaggerUiPage() {
         var content =
-                given()
-                        .basePath("/swagger-ui/index.html")
-                        .port(TestConfig.SERVER_PORT)
-                        .when()
-                        .get()
-                        .then().statusCode(200)
-                        .extract().body().asString();
+        given()
+                .basePath("/swagger-ui/index.html")
+                .port(TestConfig.SERVER_PORT)
+                .when()
+                .get()
+                .then()
+                .statusCode(200)
+                .extract()
+                .body().asString();
 
         assertTrue(content.contains("Swagger UI"));
     }
